e977ebf29591d60e8fcac7701931e3602821ccd7
Daniel Larsson <znixen@live.se>
2018-09-05 13:34:50 +0200

    Signed-off-by: Daniel Larsson <znixen@live.se> Added enum, removed various comment and unused variables.

--- src/com/codingginger/hts221/HTS221.java
+++ src/com/codingginger/hts221/HTS221.java
@@ -7,53 +7,106 @@ import java.io.IOException;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+
+
+enum range
+{
+    MIN_TEMP_C("-40f"),
+    MAX_TEMP_C("120f"),
+    MIN_HUM("0%"),
+    MAX_HUM("100%");
+
+    private String value;
+
+    range(String s) {
+        value = s;
+    }
+
+    public String getRange(){
+        return value;
+    }
+}
+
+enum regAddr
+{
+    I2C_ADDRESS(0x5f),
+    TEMP_OUT_L(0x2A),
+    TEMP_OUT_H(0x2B),
+    HUMIDITY_OUT_L(0x28),
+    HUMIDITY_OUT_H(0x29),
+    WHO_AM_I(0x0f),
+    WHO_AM_I_RETURN(0xBC),
+    REG_CTRL(0x20),
+    ODR0_SET(0x1),
+    BDU_SET(0x4),
+    POWER_MODE_ACTIVE(0x7);
+
+    private int address;
+
+    regAddr(int _address) {
+        address = _address;
+    }
+
+    public int getAddress(){
+        return address;
+    }
+}
+
+enum calib
+{
+    _h0_rH,
+    _h1_rH,
+    _T0_degC,
+    _T1_degC,
+    _H0_T0,
+    _H1_T0,
+    _T0_OUT,
+    _T1_OUT;
+
+    private char c;
+    private int i;
+
+    public void setC(char CHAR) {
+        this.c = CHAR;
+    }
+
+    public void setI(int INT) {
+        this.i = INT;
+    }
+
+    public char getC() {
+        return c;
+    }
+
+    public int getI() {
+        return i;
+    }
+}
 /**
- * @author Daniel Larsson <znixen@live.se>
+ * @author Daniel Larsson <znixen@live.se> partionally based/influesed on Pololu Arduino library for Pololu LPS25H and LPS331AP boards
  * @version 1.0
  */
 public class HTS221 implements AutoCloseable {
 
     private I2cDevice mDevice;
-
-    // static variables
-    public static final int I2C_ADDRESS =  0x5f; // HTS221 I2C address
-    public static final String BUS = "I2C1";
-    private static final int WHO_AM_I = 0x0f; // 0b1111
-    private static final int WHO_AM_I_RETURN = 0xBC; // 0b10111100
-    // Temperature register address in hex
-    private static final int TEMP_OUT_L = 0x2A;
-    private static final int TEMP_OUT_H = 0x2B;
-    // Humidity register address in hex
-    private static final int HUMIDITY_OUT_L = 0x28;
-    private static final int HUMIDITY_OUT_H = 0x29;
-    private static final int REG_CTRL = 0x20;
-    private static final int POWER_MODE_ACTIVE = 0x7;
-    private static final int ODR0_SET = 0x1; // setting sensor spreading to 1Hz
-    private static final int BDU_SET = 0x4;
-    private static final float MIN_TEMP_C = -40f; // Minimum temperature for this chip
-    private static final float MAX_TEMP_C = 120f; // Maximum temperature for this chip
+    private static final String BUS = "I2C1";
     private static final int CALIB_START = 0x30; // Calibration start
     private static final int CALIB_END = 0x3F; // Calibration ends
-    // Declare various non-static variables
-    public boolean on;
     private int mMode;
-    char _h0_rH, _h1_rH;
-    int  _T0_degC, _T1_degC, _H0_T0, _H1_T0, _T0_OUT, _T1_OUT;
+    //private char _h0_rH, _h1_rH;
+    //private int  _T0_degC, _T1_degC, _H0_T0, _H1_T0, _T0_OUT, _T1_OUT;
 
     @Retention(RetentionPolicy.SOURCE)
-    //@IntDef({MODE_DOWN, MODE_ACTIVE})
-    public @interface Mode {}
-    public static final int MODE_DOWN = ~0b10000000; //0x80
-    public static final int MODE_ACTIVE = 0b10000000; //0x80
+            //@IntDef({MODE_DOWN, MODE_ACTIVE})
+    @interface Mode {}
+    private static final int MODE_DOWN = ~0b10000000; //~0x80
+    private static final int MODE_ACTIVE = 0b10000000; //0x80
 
-    /**
-     * @author Daniel Larsson <znixen@live.se>
-     * */
-    public HTS221(){
+    HTS221(){
         PeripheralManager pioService = PeripheralManager.getInstance();
         I2cDevice device = null;
         try {
-            device = pioService.openI2cDevice(BUS, I2C_ADDRESS);
+            device = pioService.openI2cDevice(BUS, regAddr.I2C_ADDRESS.getAddress());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -69,106 +122,106 @@ public class HTS221 implements AutoCloseable {
     }
 
     /**
-     * @author Daniel Larsson <znixen@live.se>
      * @param device takes an I2CDevice as a parameter
      * */
     private void connect(I2cDevice device) throws IOException {
         mDevice = device;
-        on = pOn(MODE_ACTIVE);
+        boolean on = pOn(MODE_ACTIVE);
+        whoAmI(on);
         dbuOn();
     }
 
     /**
-     * @author Daniel Larsson <znixen@live.se>
      * @param on is a boolean
      */
-    public void whoAmI(boolean on) throws IOException {
-        if (on == true){
-            System.out.println("It´s on!");
-            int data = mDevice.readRegByte(WHO_AM_I) & 0xff;
-            System.out.println("data            WHO_AM_I_RETURN");
-            if (data == WHO_AM_I_RETURN){
-                System.out.println(data + "     " + WHO_AM_I_RETURN);
+    private void whoAmI(boolean on) throws IOException {
+        if (on){
+            int data = mDevice.readRegByte(regAddr.WHO_AM_I.getAddress()) & 0xff;
+            if (data == regAddr.WHO_AM_I_RETURN.getAddress()){
                 storeCalib();
             }else{
-                int data2 = mDevice.readRegByte(WHO_AM_I_RETURN) & 0xff;
-                System.out.println(data +  "    " + data2);
+                mDevice.readRegByte(regAddr.WHO_AM_I_RETURN.getAddress());
             }
         }else{
             System.out.println("Error: Device not on");
         }
     }
 
-    /**
-     * @author Daniel Larsson <znixen@live.se>
-     * @return returns a boolean
-     */
-    public boolean storeCalib() throws IOException {
+    private void storeCalib() throws IOException {
         int data;
         int temp;
+        int temp2;
         for (int reg = CALIB_START; reg <= CALIB_END; reg++){
             if((reg != CALIB_START+8) && (reg != CALIB_START+9) &&(reg != CALIB_START+4)){
                 data = readSample(reg);
                 switch (reg){
                     case CALIB_START:
-                        _h0_rH = (char)data;
+                        calib._h0_rH.setC((char)data);
                         break;
                     case CALIB_START+1:
-                        _h1_rH = (char)data;
+                        calib._h1_rH.setC((char)data);
                         break;
                     case CALIB_START+2:
-                        _T0_degC = data;
+                        calib._T0_degC.setI(data);
                         break;
                     case CALIB_START+3:
-                        _T1_degC = data;
+                        calib._T1_degC.setI(data);
                         break;
                     case CALIB_START+5:
-                        temp = _T0_degC;
-                        _T0_degC = (data&0x3)<<8;
-                        _T0_degC |= temp;
+                        temp = calib._T0_degC.getI();
+                        temp2 = (data&0x3)<<8;
+                        temp2 |= temp;
+                        calib._T0_degC.setI(temp2);
 
-                        temp = _T1_degC;
-                        _T1_degC = ((data&0xC)>>2)<<8;
-                        _T1_degC |= temp;
+                        temp = calib._T1_degC.getI();
+                        temp2 = ((data&0xC)>>2)<<8;
+                        temp2 |= temp;
+                        calib._T1_degC.setI(temp2);
                         break;
                     case CALIB_START+6:
-                        _H0_T0 = data;
+                        calib._H0_T0.setI(data);
                         break;
                     case CALIB_START+7:
-                        _H0_T0 |= data<<8;
+                        temp = calib._H0_T0.getI();
+                        temp |= data<<8;
+                        calib._H0_T0.setI(temp);
                         break;
                     case CALIB_START+0xA:
-                        _H1_T0 = data;
+                        calib._H1_T0.setI(data);
                         break;
                     case CALIB_START+0xB:
-                        _H1_T0 |= data <<8;
+                        temp = calib._H1_T0.getI();
+                        temp |= data <<8;
+                        calib._H1_T0.setI(temp);
                         break;
                     case CALIB_START+0xC:
-                        _T0_OUT = data;
+                        calib._T0_OUT.setI(data);
                         break;
                     case CALIB_START+0xD:
-                        _T0_OUT |= data << 8;
+                        temp = calib._T0_OUT.getI();
+                        temp |= data << 8;
+                        calib._T0_OUT.setI(temp);
                         break;
                     case CALIB_START+0xE:
-                        _T1_OUT = data;
+                        calib._T1_OUT.setI(data);
                         break;
                     case CALIB_START+0xF:
-                        _T1_OUT |= data << 8;
+                        temp = calib._T1_OUT.getI();
+                        temp |= data << 8;
+                        calib._T1_OUT.setI(temp);
                         break;
                     case CALIB_START+8:
                     case CALIB_START+9:
                     case CALIB_START+4:
                         break;
                     default:
-                        return false;
+                        return;
                 }
             }
         }
-        return true;
     }
 
     /**
-     * @author Daniel Larsson <znixen@live.se>
      * @param mode as a parameter from @Mode Interface
      * return returns a boolean
      */
@@ -176,62 +229,51 @@ public class HTS221 implements AutoCloseable {
         if (mDevice == null) {
             throw new IllegalStateException("I2C device not open");
         }
-        int regCtrl = mDevice.readRegByte(REG_CTRL) & 0xff;
+        int regCtrl = mDevice.readRegByte(regAddr.REG_CTRL.getAddress()) & 0xff;
         if (mode == MODE_DOWN){
-            regCtrl &= ~POWER_MODE_ACTIVE;
+            regCtrl &= ~regAddr.POWER_MODE_ACTIVE.getAddress();
         }else{
-            regCtrl |= POWER_MODE_ACTIVE;
-            regCtrl |= ODR0_SET;
+            regCtrl |= regAddr.POWER_MODE_ACTIVE.getAddress();
+            regCtrl |= regAddr.ODR0_SET.getAddress();
         }
-        mDevice.writeRegByte(REG_CTRL, (byte)(regCtrl));
+        mDevice.writeRegByte(regAddr.REG_CTRL.getAddress(), (byte)(regCtrl));
         mMode = mode;
-        System.out.println("Powermode on");
-        whoAmI(on);
         return true;
     }
 
-    /**
-     * @author Daniel Larsson <znixen@live.se>
-     */
-    public void dbuOn()throws IOException{
+    private void dbuOn()throws IOException{
         int data;
 
-        data = mDevice.readRegByte(REG_CTRL) & 0xff;
-        data |= BDU_SET;
-        mDevice.writeRegByte(REG_CTRL, (byte)data);
-        System.out.println("BDU set to 1");
+        data = mDevice.readRegByte(regAddr.REG_CTRL.getAddress()) & 0xff;
+        data |= regAddr.BDU_SET.getAddress();
+        mDevice.writeRegByte(regAddr.REG_CTRL.getAddress(), (byte)data);
     }
 
     /**
-     * @author Daniel Larsson <znixen@live.se>
      * @throws IOException
      */
-    public void bduOff() throws IOException{
+    private void bduOff() throws IOException{
         int data;
-        data = mDevice.readRegByte(REG_CTRL) & 0xff;
-        data &= ~BDU_SET;
-        mDevice.writeRegByte(REG_CTRL, (byte)data);
-        System.out.println("BDU set to 0");
+        data = mDevice.readRegByte(regAddr.REG_CTRL.getAddress()) & 0xff;
+        data &= ~regAddr.BDU_SET.getAddress();
+        mDevice.writeRegByte(regAddr.REG_CTRL.getAddress(), (byte)data);
     }
 
     /**
-     * @author Daniel Larsson <znixen@live.se>
      * @param mode as a parameter from @Mode Interface
      * @throws IOException
      */
-    public void pOff(@Mode int mode) throws IOException {
+    private void pOff(@Mode int mode) throws IOException {
         if (mDevice == null) {
             throw new IllegalStateException("I2C device not open");
         }
-        int regCtrl = mDevice.readRegByte(REG_CTRL) & 0xff;
-        regCtrl &= ~POWER_MODE_ACTIVE;
-        mDevice.writeRegByte(REG_CTRL, (byte)(regCtrl));
+        int regCtrl = mDevice.readRegByte(regAddr.REG_CTRL.getAddress()) & 0xff;
+        regCtrl &= ~regAddr.POWER_MODE_ACTIVE.getAddress();
+        mDevice.writeRegByte(regAddr.REG_CTRL.getAddress(), (byte)(regCtrl));
         mMode = mode;
-        System.out.println("Powermode off");
     }
 
     /**
-     * @author Daniel Larsson <znixen@live.se>
      * @return double
      * @throws IOException
      */
@@ -243,24 +285,22 @@ public class HTS221 implements AutoCloseable {
         int temp;
         double deg;
         double h_temp;
-        data = readSample(HUMIDITY_OUT_H);
+        data = readSample(regAddr.HUMIDITY_OUT_H.getAddress());
         temp = data << 8;  // MSB
-        data = readSample(HUMIDITY_OUT_L);
+        data = readSample(regAddr.HUMIDITY_OUT_L.getAddress());
         temp |= data;      // LSB
 
         // Decode Humidity
-        deg = ((int)(_h1_rH) - (int)(_h0_rH))/2.0;  // remove x2 multiple
+        deg = ((int)(calib._h1_rH.getC()) - (int)(calib._h0_rH.getC()))/2.0;  // remove x2 multiple
 
         // Calculate humidity in decimal of grade centigrades i.e. 15.0 = 150.
-        h_temp = ((temp - _H0_T0) * deg) /
-                (double)(_H1_T0 - _H0_T0);
-        deg    = (double)((int)_h0_rH) / 2.0; // remove x2 multiple
-        double _humidity = (deg + h_temp); // provide signed % measurement unit
-        return _humidity;
+        h_temp = ((temp - calib._H0_T0.getI()) * deg) /
+                (double)(calib._H1_T0.getI() - calib._H0_T0.getI());
+        deg    = (double)((int)calib._h0_rH.getC()) / 2.0; // remove x2 multiple
+        return (deg + h_temp);
     }
 
     /**
-     * @aurhor Daniel Larsson <znixen@live.se>
      * @return double
      * @throws IOException
      */
@@ -272,61 +312,65 @@ public class HTS221 implements AutoCloseable {
         int temp;
         double deg;
         double t_temp;
-        data = readSample(TEMP_OUT_H);
+        data = readSample(regAddr.TEMP_OUT_H.getAddress());
         temp = data << 8;
-        data = readSample(TEMP_OUT_L);
+        data = readSample(regAddr.TEMP_OUT_L.getAddress());
         temp |= data;
 
-        deg    = (double)((_T1_degC) - (_T0_degC))/8.0;
+        deg    = (double)((calib._T1_degC.getI()) - (calib._T0_degC.getI()))/8.0;
 
-        t_temp = ((temp - _T0_OUT) * deg) /
-                (double)(_T1_OUT - _T0_OUT);
-        deg    = (double)((int)_T0_degC) / 8.0;
-        double _temperature = deg + t_temp;
+        t_temp = ((temp - calib._T0_OUT.getI()) * deg) /
+                (double)(calib._T1_OUT.getI() - calib._T0_OUT.getI());
+        deg    = (double)((int)calib._T0_degC.getI()) / 8.0;
 
-        return _temperature;
+        return deg + t_temp;
     }
 
     /**
-     * @author  Daniel Larsson <znixen@live.se>
      * @param _address
-     * @return
      * @throws IOException
      */
 
     private int readSample(int _address) throws IOException {
-        int temp = mDevice.readRegByte(_address) & 0xff;
-        return temp;
+        return mDevice.readRegByte(_address) & 0xff;
     }
 
     /**
-     * @author Daniel Larsson <znixen@live.se>
-     * @return
+     * @return String min temperature
      */
-    public float getMinTempC(){
-        return MIN_TEMP_C;
+    public String getMinTempC(){
+        return range.MIN_TEMP_C.getRange();
     }
 
     /**
-     * @author Daniel Larsson <znixen@live.se>
-     * @return float max temperature
+     * @return String max temperature
      */
 
-    public float getMaxTempC(){
-        return MAX_TEMP_C;
+    public String getMaxTempC(){
+        return range.MAX_TEMP_C.getRange();
     }
 
     /**
-     * @author Daniel Larsson <znixen@live.se>
+     * @return String min humidity
+     */
+    public String getMinHum(){
+        return range.MIN_HUM.getRange();
+    }
+
+    /**
+     * @return String max humidity
+     */
+    public String getMaxHum(){
+        return range.MAX_HUM.getRange();
+    }
+
+    /**
      * @return int mMode
      */
     public int getMode(){
         return mMode;
     }
 
-    /**
-     * @author Daniel Larsson <znixen@live.se>
-     */
     @Override
     public void close(){
         if (mDevice != null){
